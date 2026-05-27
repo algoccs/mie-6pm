2b201901038cddb5831cb227577dd541946f086a
algoccs <algodemoacc@gmail.com>
2026-05-27 19:00:54 -0400

    Add plpatilla base para proyectos

---
+++ plantilla.py
@@ -0,0 +1,5 @@
+from PyQt5.QtWidgets import (
+                QApplication, QWidget, QVBoxLayout, QHBoxLayout,
+                QLabel, QPushButton, QLineEdit
+                )
+from PyQt5.QtCore import Qt
